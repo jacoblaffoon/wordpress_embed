f46dfb595cb6fc6a0c8809737df32361993aaafd
Jacob Laffoon <thomas.j.laffoon@gmail.com>
2020-03-11 09:51:46 -0500

    added code-embed.php

---
+++ code-embed.php
@@ -0,0 +1,9 @@
+<?php
+/**
+* Plugin Name: Wordpress Embed Code in Pages/Posts
+* Plugin URI: https://www.wordpress-embed-code.com/
+* Description: This is the very first plugin I ever created.
+* Version: 1.0
+* Author: Jacob Laffoon
+* Author URI: http://consideratecoder.com/
+**/
